434439a30d9b5c7d3bfe888dd9ecd73e38ab1174
Lucas Mahieu <lucas.mahieu@icloud.com>
2015-12-02 11:39:38 +0100

    Ajout du param min a tsp

--- src/tsp-tsp.c
+++ src/tsp-tsp.c
@@ -39,7 +39,7 @@ int present (int city, int hops, tsp_path_t path, uint64_t vpres)
 
 pthread_mutex_t mutex_tsp;
 pthread_mutex_t mutex_tsp2;
-void tsp (int hops, int len, uint64_t vpres, tsp_path_t path, long long int *cuts, tsp_path_t sol, int *sol_len)
+void tsp (int hops, int len, uint64_t vpres, tsp_path_t path, long long int *cuts, tsp_path_t sol, int *sol_len, int* min_loc)
 {
 	// On fait la cpy locale du minimum 
 	// et si on trouve mieux on en fera la cpy en exclu mutuelle 
